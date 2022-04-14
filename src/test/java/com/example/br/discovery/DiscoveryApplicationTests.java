8182a1679cab9b26f88ff5de224a1632896f3eaa
“rohithegde-sankey” <rohit.h@sankeysolutions.com>
2022-04-14 09:20:33 +0530

    Initial commit for eureka discovery service

---
+++ src/test/java/com/example/br/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.br.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
